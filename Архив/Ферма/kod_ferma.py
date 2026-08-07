b1403d0eacd70ec78220d0972af9f280b1d00117
artem <artem@Mac-mini-artem.local>
2026-08-08 00:21:00 +0300

    Удалён Архив из репозитория

--- Архив/Ферма/kod_ferma.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-data_ferma_main = pd.read_csv('/Users/test/Desktop/Яндекс Практикум/Проекты/Ферма/ferma_main.csv', sep=';')
-data_ferma_dad = pd.read_csv('/Users/test/Desktop/Яндекс Практикум/Проекты/Ферма/ferma_dad.csv', sep=';')
-data_cow_buy = pd.read_csv('/Users/test/Desktop/Яндекс Практикум/Проекты/Ферма/cow_buy.csv', sep=';')
-
